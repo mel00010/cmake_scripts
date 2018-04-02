987838bdda1447b950e4e5f6ef709a501ce266f6
Mel McCalla <melmccalla@gmail.com>
2018-04-02 15:36:49 -0700

    Updated DistDictionary.cmake

--- DistDictionary.cmake
+++ DistDictionary.cmake
@@ -1,10 +1,26 @@
+if (NOT DEFINED WORKSPACE_PATH)
+    set(WORKSPACE_PATH $ENV{HOME}/workspace)
+endif ()
+
+if (NOT DEFINED DICTIONARY_LOCATION)
+    set(DICTIONARY_LOCATION ${WORKSPACE_PATH}/dictionary)
+endif ()
+
+
 function(setup_dictionary_targets)
-	add_custom_target(sort_dictionary
+	configure_file(
+		${CMAKE_SOURCE_DIR}/dictionary_sorted
+		${CMAKE_BINARY_DIR}/dictionary
+	)
+	add_custom_target(sort_dictionary ALL
 		COMMAND sort ${CMAKE_BINARY_DIR}/dictionary -o ${CMAKE_SOURCE_DIR}/dictionary_sorted
 	)
-	add_custom_target(regen_dictionary
+	add_custom_target(regen_dictionary ALL
 		COMMAND ${CMAKE_COMMAND} -E copy
 				${CMAKE_SOURCE_DIR}/dictionary_sorted
                 ${CMAKE_BINARY_DIR}/dictionary
+    )
+    add_custom_target(link_dictionary ALL
+    	COMMAND ln -sf ${CMAKE_BINARY_DIR}/dictionary ${DICTIONARY_LOCATION}
     )
 endfunction()
