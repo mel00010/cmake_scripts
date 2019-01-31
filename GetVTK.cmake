de33e18ee9c97cb9eca556a916f718d6f8246407
Mel McCalla <melmccalla@gmail.com>
2019-01-30 18:14:16 -0800

    Updated GetGoogleTest.cmake, VTK.cmake.in,  and GetVTK.cmake

--- GetVTK.cmake
+++ GetVTK.cmake
@@ -1,24 +1,31 @@
-# We need thread support
-find_package (Threads REQUIRED)
+include(FetchContent)
+set(FETCHCONTENT_QUIET OFF)
 
-# Download and unpack vtk at configure time
-configure_file(${CMAKE_SOURCE_DIR}/cmake/VTK.cmake.in  ${CMAKE_BINARY_DIR}/vtk-download/CMakeLists.txt)
+FetchContent_Declare(vtk
+	GIT_REPOSITORY https://vtk.org/VTK.git
+	GIT_TAG v8.1.2
+)
 
-execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-  RESULT_VARIABLE result
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/vtk-download )
-if(result)
-  message(FATAL_ERROR "CMake step for vtk failed: ${result}")
+FetchContent_GetProperties(vtk)
+message(vtk_POPULATED)
+if(NOT vtk_POPULATED)
+	FetchContent_Populate(vtk)
+	add_subdirectory(${vtk_SOURCE_DIR} ${vtk_BINARY_DIR})
+	set(VTK_DIR ${vtk_BINARY_DIR})
+	find_package(VTK REQUIRED)
+	include("${VTK_USE_FILE}")
 endif()
-execute_process(COMMAND ${CMAKE_COMMAND} --build .
-  RESULT_VARIABLE result
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/vtk-download )
-if(result)
-  message(FATAL_ERROR "Build step for vtk failed: ${result}")
-endif()
+
+#list(APPEND CMAKE_MODULE_PATH "${VTK_DIR}/lib/cmake/vtk-8.90/")
 
 
-# Add vtk directly to our build. This defines
-# the vtk target.
-add_subdirectory(${CMAKE_BINARY_DIR}/vtk-src
-                 ${CMAKE_BINARY_DIR}/vtk-build)
+
+#message("VTK_FOUND:  ${VTK_FOUND}")
+#message("VTK_USE_FILE:  ${VTK_USE_FILE}")
+#message("VTK_INCLUDE_DIRS:  ${VTK_INCLUDE_DIRS}")
+#message("VTK_KITS:  ${VTK_KITS}")
+#message("VTK_BUILD_VERSION:  ${VTK_BUILD_VERSION}")
+#message("VTK_DIR:  ${VTK_DIR}")
+#message("VTK_MAJOR_VERSION:  ${VTK_MAJOR_VERSION}")
+#message("VTK_LIBRARY_DIRS:  ${VTK_LIBRARY_DIRS}")
+#message("VTK_LIBRARIES:  ${VTK_LIBRARIES}")
