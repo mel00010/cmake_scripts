cd312a4cf198482f7490b3db8519f5a7c179b824
Mel McCalla <melmccalla@gmail.com>
2020-05-26 13:12:16 -0700

    Added GetBoostDI.cmake

---
+++ GetBoostDI.cmake
@@ -0,0 +1,14 @@
+include(FetchContent)
+set(FETCHCONTENT_QUIET OFF)
+
+FetchContent_Declare(BoostDI
+  GIT_REPOSITORY  https://github.com/boost-experimental/di.git
+  GIT_TAG         cpp14
+)
+
+FetchContent_GetProperties(BoostDI)
+if(NOT BoostDI_POPULATED)
+  FetchContent_Populate(BoostDI)
+  cmake_policy(SET CMP0069 NEW)
+  add_subdirectory("${boostdi_SOURCE_DIR}" "${boostdi_BINARY_DIR}")
+endif()
